6b2c25adde48316ea3d6495ca67ff69243b63151
Alexis Do Nascimento <alexis.dona@gmail.com>
2022-05-01 00:20:25 -0300

    Agrego estructura de pcb para kernel

--- kernel/src/kernel.h
+++ kernel/src/kernel.h
@@ -7,6 +7,15 @@
 #include <commons/log.h>
 #include "utils.h"
 
+typedef struct {
+	uint32_t proceso_id;
+	uint32_t tamanio_proceso;
+	t_list* listaInstrucciones;
+	uint32_t program_counter;
+	uint32_t tabla_paginas;
+	uint32_t estimacion_rafaga;
+} t_pcb;
+
 int validar_y_ejecutar_opcion_consola(int opcion, int consola_fd, int kernel_fd);
 int recibir_opcion();
 int accion_kernel(int consola_fd, int kernel_fd);
