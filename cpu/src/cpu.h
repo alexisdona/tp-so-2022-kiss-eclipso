a60a0b2be9a1413908420cf03c69bc82a191ebfc
matiferraro <matiasaferraro@gmail.com>
2022-05-13 20:08:49 -0300

    Agrego base modulo cpu

---
+++ cpu/src/cpu.h
@@ -0,0 +1,19 @@
+#ifndef SRC_CPU_H_
+#define SRC_CPU_H_
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<commons/log.h>
+#include<commons/string.h>
+#include<commons/config.h>
+#include<readline/readline.h>
+#include<commons/collections/list.h>
+
+#include "include/utils.h"
+
+#define LOG_NAME "CPU_LOG"
+#define LOG_FILE "cpu.log"
+#define CONFIG_FILE "../src/config/cpu.config"
+
+#endif /* SRC_CPU_H_ */
---
+++ cpu/src/include/utils.h
@@ -0,0 +1,33 @@
+#ifndef UTILS_H_
+#define UTILS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <commons/log.h>
+#include <commons/collections/list.h>
+#include <string.h>
+#include <assert.h>
+
+#include "protocolo.h"
+
+#define IP "127.0.0.1"
+#define PUERTO "8000"
+#define SIN_CONSOLAS -1
+
+t_log* logger;
+
+void* recibir_buffer(int*, int);
+
+int iniciar_kernel(void);
+int esperar_consola(int);
+t_list* recibir_paquete(int);
+void recibir_mensaje(int);
+int recibir_operacion(int);
+
+void verificarBind(int, const struct addrinfo *);
+void verificarListen(int);
+
+#endif /* UTILS_H_ */
