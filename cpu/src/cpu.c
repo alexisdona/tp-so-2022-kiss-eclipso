76211bc22dd135c23b0eac6375a9c1128dc13c8b
matiferraro <matiasaferraro@gmail.com>
2022-05-23 21:33:54 -0300

    cambios cpu

--- cpu/src/cpu.c
+++ cpu/src/cpu.c
@@ -25,3 +25,51 @@ int main(void) {
 	}
 	return EXIT_SUCCESS;
 }
+
+int recibir_opcion() {
+	char* opcion = malloc(4);
+	log_info(logger, "Ingrese una opcion: ");
+	scanf("%s",opcion);
+	int op = atoi(opcion);
+	free(opcion);
+	return op;
+}
+
+int validar_y_ejecutar_opcion_memoria(int opcion, int memoria_fd, int cpu_fd) {
+
+	int continuar=1;
+
+	switch(opcion) {
+		case 1:
+			log_info(logger,"cpu continua corriendo, esperando nueva memoria.");
+			memoria_fd = esperar_memoria(cpu_fd);
+			break;
+		case 0:
+			log_info(logger,"Terminando cpu...");
+			close(cpu_fd);
+			continuar = 0;
+			break;
+		default:
+			log_error(logger,"Opcion invalida. Volve a intentarlo");
+			recibir_opcion();
+	}
+
+	return continuar;
+
+}
+
+int accion_cpu(int memoria_fd, int cpu_fd) {
+
+	log_info(logger, "¿Desea mantener el kernel corriendo? 1- Si 0- No");
+	int opcion = recibir_opcion();
+
+	if (recibirOperacion(memoria_fd) == SIN_CONSOLAS) {
+	  log_info(logger, "No hay mas clientes conectados.");
+	  log_info(logger, "¿Desea mantener el kernel corriendo? 1- Si 0- No");
+	  opcion = recibir_opcion();
+	  return validar_y_ejecutar_opcion_memoria(opcion, memoria_fd, cpu_fd);
+	}
+
+	return validar_y_ejecutar_opcion_memoria(opcion, memoria_fd, cpu_fd);
+
+}
--- cpu/src/include/utils.c
+++ cpu/src/include/utils.c
@@ -68,3 +68,20 @@ op_code recibirOperacion(int socket_memoria) {
         return -1;
     }
 }
+
+void* recibirBuffer(size_t size, int socket_memoria)
+{
+	void * buffer;
+	buffer = malloc(size);
+	recv(socket_memoria, buffer, size, MSG_WAITALL);
+
+	return buffer;
+}
+
+void recibirMensaje(int socket_memoria)
+{
+	int size;
+	char* buffer = recibirBuffer(size, socket_memoria);
+	log_info(logger, "Me llego el mensaje %s", buffer);
+	free(buffer);
+}
